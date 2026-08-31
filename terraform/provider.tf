92d841c61b2019f119769c0321d75f8de5ee16f5
Yash Wairagade <yashwairagade99@gmail.com>
2026-08-31 11:44:05 +0530

    remote state config

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,21 +1,14 @@
 terraform {
   required_version = ">= 1.0"
-  
+
+  backend "s3" {}
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-
-  # Uncomment the following block and replace with your S3 bucket for remote state management
-  # backend "s3" {
-  #   bucket         = "your-terraform-state-bucket"
-  #   key            = "myapp/terraform.tfstate"
-  #   region         = "us-east-1"
-  #   encrypt        = true
-  #   dynamodb_table = "terraform-lock"
-  # }
 }
 
 provider "aws" {
