58818709a4433dac456034bca85d54c831491fb4
Yash Wairagade <yashwairagade99@gmail.com>
2026-09-01 18:08:22 +0530

    comments

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -8,7 +8,7 @@ resource "aws_db_subnet_group" "main" {
   }
 }
 
-# RDS Instance
+# RDS Instances
 resource "aws_db_instance" "main" {
   identifier            = "${var.app_name}-db-${var.environment}"
   engine                = "postgres"
