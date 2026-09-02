076c1cfc138cc24cff23959242f4b033dd07b078
Yash Wairagade <yashwairagade99@gmail.com>
2026-09-02 15:43:48 +0530

    comments

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -14,7 +14,7 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-# IAM Role for EC2 instances
+# IAM Role for EC2 instance
 resource "aws_iam_role" "ec2_role" {
   name = "${var.app_name}-ec2-role-${var.environment}"
 
